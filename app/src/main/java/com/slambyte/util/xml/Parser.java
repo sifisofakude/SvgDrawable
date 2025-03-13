ab29ee61d40042e546dc5ffb1a6df571230e065c
sifisofakude <slamfaku@gmail.com>
2025-03-13 06:57:35 +0200

    Commit From App Compilation

--- app/src/main/java/com/slambyte/util/xml/Parser.java
+++ app/src/main/java/com/slambyte/util/xml/Parser.java
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.LineNumberReader;
 import java.io.InputStreamReader;
 
-import java.nio.fuile.Path;
+import java.nio.file.Path;
 
 import java.util.ArrayList;
 
