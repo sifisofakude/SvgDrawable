abe2ccafca52453d5e86439a556f322479ea9060
sifisofakude <slamfaku@gmail.com>
2025-03-13 07:28:58 +0200

    Commit From App Compilation

--- app/src/main/java/com/slambyte/util/xml/Parser.java
+++ app/src/main/java/com/slambyte/util/xml/Parser.java
@@ -110,7 +110,7 @@ public class Parser	{
 			// System.out.println(doc.docStr);
 			if(options.indexOf("--external-gradients") > -1)	{
 				Path path = new File(output).toPath();
-				output = path.getParent().toString()+"/res/drawable/"+path.getFilename();
+				output = path.getParent().toString()+"/res/drawable/"+path.getFileName();
 			}
 			doc.writeToFile(output);
 		}catch(IOException e)	{}
