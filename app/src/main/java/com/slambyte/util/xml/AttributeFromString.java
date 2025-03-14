3b758e4359b6dd6f568d2db0dcaa866d69b3d4c5
sifisofakude <slamfaku@gmail.com>
2025-03-14 12:12:49 +0200

    Commit From App Compilation

--- app/src/main/java/com/slambyte/util/xml/AttributeFromString.java
+++ app/src/main/java/com/slambyte/util/xml/AttributeFromString.java
@@ -1,6 +1,5 @@
 package com.slambyte.util.xml;
 
-import java.util.Iterator;
 import java.util.ArrayList;
 
 public class AttributeFromString	{
--- app/src/main/java/com/slambyte/util/xml/Document.java
+++ app/src/main/java/com/slambyte/util/xml/Document.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.PrintStream;
 import java.io.IOException;
 
+import java.util.Iterator;
 import java.util.ArrayList;
 
 public class Document	{
